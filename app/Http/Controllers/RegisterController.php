998e46f2e3e75c91e4646cc26be9dfb1b935d887
JulianAVG64 <julianavg2002@gmail.com>
2025-04-19 14:29:44 -0500

    correction RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -30,19 +30,19 @@ class RegisterController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
 
-        // // Crear registro
-        // User::create([
-        //     'name' => $request->name,
-        //     'username' => $request->username,
-        //     'email' => $request->email,
-        //     'password' => Hash::make( $request->password ) 
-        // ]);
-
-        // Autenticar un usuario
-        auth()->attempt([
+        // Crear registro
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
-            'password' => $request->password
+            'password' => Hash::make( $request->password ) 
         ]);
+
+        // // Autenticar un usuario
+        // auth()->attempt([
+        //     'email' => $request->email,
+        //     'password' => $request->password
+        // ]);
 
         // Otra forma de autenticar
         auth()->attempt($request->only('email', 'password'));
