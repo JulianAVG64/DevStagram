302d1fd41c9e216f1d30e89a1cdb83c7093c22e0
JulianAVG64 <julianavg2002@gmail.com>
2025-04-22 18:41:49 -0500

    Implement another way to create a record in PostController using Eloquent relations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,20 +36,30 @@ class PostController extends Controller
         ]);
 
         // Crear registro de publicación con el id de la sesión actual
-        Post::create([
-            'titulo' => $request->titulo,
-            'descripcion' => $request->descripcion,
-            'imagen' => $request->imagen,
-            'user_id' => auth()->user()->id
-        ]);
+        // Post::create([
+        //     'titulo' => $request->titulo,
+        //     'descripcion' => $request->descripcion,
+        //     'imagen' => $request->imagen,
+        //     'user_id' => auth()->user()->id
+        // ]);
 
-        // // Otra forma de crear el registro
+
+        // Otra forma de crear el registro
         // $post = new Post;
         // $post->titulo = $request->titulo;
         // $post->descripcion = $request->descripcion;
         // $post->imagen = $request->imagen;
         // $post->user_id = auth()->user()->id;
         // $post->save();
+
+        
+        // Otra forma de crear el registro usando las relaciones de Eloquent
+        $request->user()->posts()->create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
 
         // Redireccionar
         return redirect()->route('posts.index', auth()->user()->username);
