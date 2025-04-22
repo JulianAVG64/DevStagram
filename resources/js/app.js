1b27d1996ddecfda7bf7dfed96319bb7b7b4fea2
JulianAVG64 <julianavg2002@gmail.com>
2025-04-22 13:42:26 -0500

    Posts validation kepping the image on the form

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,18 +9,27 @@ const dropzone = new Dropzone('#dropzone', {
     dictRemoveFile: 'Borrar Archivo',
     maxFiles: 1,
     uploadMultiple: false,
-});
 
-dropzone.on('sending', function(file, xhr, formData) {
-    console.log(formData);
+    // Función para seguir mostrando la imágen si el formulario da errores
+    init: function() {
+        if(document.querySelector('[name="imagen"]').value.trim()) {
+            const imagenPublicada = {};
+            imagenPublicada.size = 1234;
+            imagenPublicada.name = document.querySelector('[name="imagen"]').value;
+
+            this.options.addedfile.call( this, imagenPublicada );
+            this.options.thumbnail.call( this, imagenPublicada, `/uploads/${imagenPublicada.name}`);
+
+            imagenPublicada.previewElement.classList.add('dz-success', 'dz-complete');
+        }
+    },
 });
 
 dropzone.on('success', function(file, response) {
-    console.log(response);
+    document.querySelector('[name="imagen"]').value = response.imagen;
 });
 
-dropzone.on('error', function(file, message) {
-    console.log(message);
-});
 
-dropzone.on('removedfile', function() {});
+dropzone.on('removedfile', function() {
+    document.querySelector('[name="imagen"]').value = "";
+});
