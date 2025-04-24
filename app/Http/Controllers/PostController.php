20044033a2a34b5491b8bb4d42867131b5c9de1e
JulianAVG64 <julianavg2002@gmail.com>
2025-04-24 15:00:40 -0500

    Deleting post image

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -82,6 +83,13 @@ class PostController extends Controller
         // Proteger la acción por medio de una política
         $this->authorize('delete', $post);
         $post->delete();
+
+        // Eliminar la imagen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+
+        if(File::exists($imagen_path)) {
+            unlink($imagen_path);
+        }
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
