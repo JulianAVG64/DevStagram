45c981188aff7717f15142e9683982d3feeca256
JulianAVG64 <julvargasgu@unal.edu.co>
2025-04-25 10:00:19 -0500

    Store comment in db

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        // 'post_id' Ya no se necesita por la relación de Eloquent que hay en el modelo de Post
+    ];
 }
