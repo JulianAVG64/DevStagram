f6ab2ca87b8cf7c3a74286dc3f43f84fc6961298
JulianAVG64 <julianavg2002@gmail.com>
2025-04-29 19:38:35 -0500

    Migration for image field in users table

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -53,7 +53,7 @@ class PerfilController extends Controller
         // Guardar cambios
         $usuario = User::find(auth()->user()->id);
         $usuario->username = $request->username;
-        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? '';
         $usuario->save();
 
         // Redireccionar al usuario
