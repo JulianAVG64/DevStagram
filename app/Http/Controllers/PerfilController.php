6649a94f81268a3441f53d1a00196fd10fc01df8
JulianAVG64 <julianavg2002@gmail.com>
2025-04-29 19:30:18 -0500

    Saving user profile changes

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class PerfilController extends Controller
 {
@@ -29,5 +31,32 @@ class PerfilController extends Controller
         $this->validate($request, [
             'username' => ['required', 'unique:users,username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter,editar-perfil']
         ]);
+
+        if($request->imagen) {
+            $imagen = $request->file('imagen');
+
+            // Generar nombre unico de imagen
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+    
+            // Crear instancia de imagen usando Intervention Image, va a estar un rato en memoria
+            $manager = new ImageManager(Driver::class);
+            $imagenServidor = $manager->read($imagen);
+    
+            // Modificar la imagen
+            $imagenServidor->cover(1000, 1000);
+    
+            // Mover la imagen al servidor
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+
+        // Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+
+        // Redireccionar al usuario
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
